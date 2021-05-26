85ee7e9ae2a679c809680f991c77ab69e36bdc5b
ahmedthahir <ahmedthahir369@gmail.com>
2021-05-26 18:37:06 +0530

    third commit: added dummy test cases

--- src/test/java/com/helloWorld/CiCdDemoApplicationTests.java
+++ src/test/java/com/helloWorld/CiCdDemoApplicationTests.java
@@ -2,12 +2,20 @@ package com.helloWorld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiCdDemoApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void testcase1(){
+		assertEquals(1,1);
+	}
+
+	@Test
+	void testcase2(){
+		assertEquals("hai","hai");
 	}
 
 }
